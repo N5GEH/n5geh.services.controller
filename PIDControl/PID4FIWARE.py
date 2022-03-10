c6f8a7f8f7e8280ddbaa1cfaa1cfe56d5f1a284a
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2022-03-10 16:07:41 +0100

    Replace self coded controller with simple-pid package

--- PIDControl/PID4FIWARE.py
+++ PIDControl/PID4FIWARE.py
@@ -10,6 +10,7 @@ PID controller with Fiware interface
 import time
 import os
 import PIDcontroller
+from simple_pid import PID
 import requests
 
 from filip.clients.ngsi_v2 import ContextBrokerClient
@@ -19,9 +20,11 @@ from filip.models.ngsi_v2.context import NamedCommand, ContextEntity, NamedConte
 
 # TODO
 #  1. how to let controller state pending (if the sensor fails)
+#     shut down the controller and keep the container alive? or just let the container die?
 #  2. how to tune the control parameters during run time? The parameters can be change by sending requests to CB.
-#  Maybe another container?
+#     Maybe another container?
 #  3. write a small tutorial in this
+#  4. switch to simple-pid
 class Control:
     """PID controller with FIWARE interface"""
 
@@ -43,6 +46,7 @@ class Control:
         self.params['Td'] = float(os.getenv("TD", '0'))
         self.params['lim_low'] = float(os.getenv("LIM_LOW", '0'))
         self.params['lim_high'] = float(os.getenv("LIM_HIGH", '100'))
+        # TODO remove this parameter? reverse mode can be activate by passing negative tunings to controller
         self.params['reverse_act'] = 'True' if os.getenv("REVERSE_ACT", 'False') == 'True' or \
                                                os.getenv("REVERSE_ACT", 'False') == 'true' or \
                                                os.getenv("REVERSE_ACT", 'False') == 'TRUE' \
@@ -68,6 +72,14 @@ class Control:
                                      self.params['lim_low'],
                                      self.params['lim_high'],
                                      self.params['reverse_act'])
+        # simple pid instance
+        # TODO use the reverse for the tuning parameters, or change the env parameters to Ki and Kd
+        self.pid = PID(self.params['Kp'], self.params['Ti'], self.params['Td'],
+                       setpoint=self.params['setpoint'],
+                       sample_time=self.params['pause_time'],
+                       output_limits=(self.params['lim_low'], self.params['lim_high'])
+                       )
+
 
         # Create the header
         fiware_header = FiwareHeader(service=self.params['service'], service_path=self.params['service_path'])
@@ -92,7 +104,7 @@ class Control:
             # TODO better way to deal with and entity name?
             #  For example: heater(actuator_device)_pid(algorithms)_controller_1(random_number?)
             # TODO entity type is now hard coded to PID_Controller
-            pid = ContextEntity(id=f'{self.params["name"]}',
+            pid_entity = ContextEntity(id=f'{self.params["name"]}',
                                 type=self.params['type'])
             cb_attrs = []
             for attr in ['Kp', 'Ti', 'Td', 'lim_low', 'lim_high', 'setpoint']:
@@ -103,13 +115,14 @@ class Control:
             cb_attrs.append(NamedContextAttribute(name="reverse_act",
                                                   type="Text",
                                                   value=self.params["reverse_act"]))
-            pid.add_attributes(attrs=cb_attrs)
-            self.ORION_CB.post_entity(entity=pid, update=True)
+            pid_entity.add_attributes(attrs=cb_attrs)
+            self.ORION_CB.post_entity(entity=pid_entity, update=True)
 
     def update_params(self):
         """Read PID parameters of entity in context broker and updates PID control parameters"""
 
         # read parameters from context broker
+        # TODO if the request fails, shut down the controller and keep the container alive? or just let the container die?
         for attr in ['Kp', 'Ti', 'Td', 'lim_low', 'lim_high', 'setpoint']:
             self.params[attr] = float(
                 self.ORION_CB.get_attribute_value(entity_id=self.params['name'],
@@ -123,13 +136,16 @@ class Control:
         print(f"reverse_act = ({self.params['reverse_act']})")
 
         # update PID parameters
-        self.PID.Kp = self.params['Kp']
-        self.PID.Ti = self.params['Ti']
-        self.PID.Td = self.params['Td']
-        self.PID.lim_low = self.params['lim_low']
-        self.PID.lim_high = self.params['lim_high']
-        # TODO how to deal with this parameters
-        self.PID.reverse_act = bool(int(self.params['reverse_act']))
+        # self.PID.Kp = self.params['Kp']
+        # self.PID.Ti = self.params['Ti']
+        # self.PID.Td = self.params['Td']
+        # self.PID.lim_low = self.params['lim_low']
+        # self.PID.lim_high = self.params['lim_high']
+        # # TODO how to deal with this parameters, remove it?
+        # self.PID.reverse_act = bool(int(self.params['reverse_act']))
+        self.pid.tunings = (self.params['Kp'], self.params['Ti'], self.params['Td'])
+        self.pid.output_limits = (self.params['lim_low'], self.params['lim_high'])
+        self.pid.setpoint = self.params['setpoint']
 
     def run(self):
         """Calculation of PID output"""
@@ -165,7 +181,8 @@ class Control:
         self.x_act = float(x)
         # calculate PID output
         print(f"x_act ={self.x_act}, x_set = {self.params['setpoint']}")
-        self.y = self.PID.run(x_act=self.x_act, x_set=self.params['setpoint'])
+        # self.y = self.PID.run(x_act=self.x_act, x_set=self.params['setpoint'])
+        self.y = self.pid(self.x_act)
         # build command
         command = NamedCommand(name=self.params['actuator_command'], value=round(self.y, 3))
         # send post command
@@ -175,30 +192,14 @@ class Control:
         self.ORION_CB.post_command(entity_id=self.params['actuator_entity_name'],
                                    entity_type=self.params['actuator_type'],
                                    command=command)
-        print(f"heater power: {self.y}")
+        print(f"output: {self.y}")
 
 
 if __name__ == "__main__":
-    # os.environ["CONFIG_FILE"] = "False"
-    # os.environ["ORION_HOST"] = "http://137.226.249.254"
-    # os.environ['FIWARE_SERVICE'] = "simple_iot"
-    # os.environ['FIWARE_SERVICE_PATH'] = "/"
-    # os.environ['SENSOR_ENTITY_NAME'] = "urn:ngsi-ld:Sensor:T1"
-    # os.environ['SENSOR_ATTRS'] = "urn:ngsi-ld:Measurement:Temperature"
-    # os.environ['ACTUATOR_ENTITY_NAME'] = "urn:ngsi-ld:Actuator:valve1"
-    # os.environ['ACTUATOR_TYPE'] = "Actuator"
-    # os.environ['ACTUATOR_COMMAND'] = "valveSet"
-
-    PID = Control()
-    PID.create_entity()
-
-    # while True:
-    #     print("Hello Docker")
-    #     time.sleep(10)
-
-    # PID.ORION_CB.update_attribute('PID_rh', 'setpoint', 298.15)
+    pid_controller = Control()
+    pid_controller.create_entity()
     while True:
-        #  for i in range(100):
-        PID.update_params()
-        PID.run()
-        time.sleep(PID.params['pause_time'])
+        pid_controller.update_params()
+        pid_controller.run()
+        # TODO the sample time is also passed to the pid instance, do we really need to pass it twice? or only here?
+        time.sleep(pid_controller.params['pause_time'])
